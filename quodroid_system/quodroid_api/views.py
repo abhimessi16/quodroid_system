f7e66cf95a2a8b3fa3cebfdccc3f6ffba0aa8770
abhimessi16 <piabhi18@gmail.com>
2024-04-11 21:53:36 +0530

    removing cache files, ig

--- quodroid_system/quodroid_api/views.py
+++ quodroid_system/quodroid_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import TestsSerializer
 
+import os
+
 @api_view(['POST'])
 def run_robot_test(request):
     serializer = TestsSerializer(data=request.data)
@@ -10,5 +12,8 @@ def run_robot_test(request):
     if serializer.is_valid():
 
         print(serializer.data)
+
+        suites_count = len(os.listdir(os.getcwd() + "\\quodroid_api\\test_suites"))
+
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
