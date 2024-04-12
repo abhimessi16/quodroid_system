df725f6abaaa115284e54deab974435c58d7d9ba
abhimessi16 <piabhi18@gmail.com>
2024-04-13 00:44:54 +0530

    addig output files in seperate folder for each api call

---
+++ quodroid_system/quodroid_api/test_suites/test_suite_1/test.robot
@@ -0,0 +1,14 @@
+
+*** Settings ***
+
+Library  SeleniumLibrary
+
+*** Test Cases ***
+
+Open google.com
+   Open Browser  browser=chrome
+   Go To  url=https://google.com
+
+Open yahoo.com
+   Open Browser  browser=chrome
+   Go To  url=https://yahoo.com
--- quodroid_system/quodroid_api/views.py
+++ quodroid_system/quodroid_api/views.py
@@ -2,29 +2,32 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import TestsSerializer
-from .models import Tests
 
 import os
 
+# could be some cloud location
 test_suites_location = os.getcwd() + "\\quodroid_api\\test_suites"
 
+# POST method
 @api_view(['POST'])
 def run_robot_test(request):
+
     serializer = TestsSerializer(data=request.data)
 
     if serializer.is_valid():
 
-        print(serializer.data)
-
         suites_count = len(os.listdir(test_suites_location))
 
         # instead of counting the files we can use a unique name for each suite file
+        # counting since working locally for now
         new_suite_name = f'{test_suites_location}\\test_suite_{suites_count + 1}'
+        os.mkdir(new_suite_name)
+        new_testfile_name = f"{new_suite_name}\\test"
 
         suite_content = f"""
 *** Settings ***
 
-Library SeleniumLibrary
+Library  SeleniumLibrary
 
 *** Test Cases ***"""
         tests = serializer.data.get("tests", [])
@@ -39,11 +42,14 @@ Library SeleniumLibrary
 
             suite_content += test_case
             
-        with open(f"{new_suite_name}.robot", 'w') as robot_suite:
+        with open(f"{new_testfile_name}.robot", 'w') as robot_suite:
             robot_suite.write(suite_content)
-            pass
+        
+        print(test_suites_location)
+        
+        os.system(f'robot -d {new_suite_name} {new_testfile_name}.robot')
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'results': new_suite_name}, status=status.HTTP_200_OK)
     else:
         # steps are mandatory
         return Response("Incorrect data format", status=status.HTTP_400_BAD_REQUEST)
