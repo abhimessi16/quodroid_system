d9a0d4a5d40733c54cf6003c93f2f1847e532455
abhimessi16 <piabhi18@gmail.com>
2024-04-12 20:39:06 +0530

    creation of suite file

--- quodroid_system/quodroid_api/views.py
+++ quodroid_system/quodroid_api/views.py
@@ -5,6 +5,8 @@ from .serializers import TestsSerializer
 
 import os
 
+test_suites_location = os.getcwd() + "\\quodroid_api\\test_suites"
+
 @api_view(['POST'])
 def run_robot_test(request):
     serializer = TestsSerializer(data=request.data)
@@ -13,7 +15,14 @@ def run_robot_test(request):
 
         print(serializer.data)
 
-        suites_count = len(os.listdir(os.getcwd() + "\\quodroid_api\\test_suites"))
+        suites_count = len(os.listdir(test_suites_location))
+
+        # instead of counting the files we can use a unique name for each suite file
+        new_suite_name = f'{test_suites_location}\\test_suite_{suites_count + 1}'
+        
+        with open(f"{new_suite_name}.robot", 'w') as robot_suite:
+            robot_suite.write("data")
+            pass
 
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
